49e7d62ad95c2c90a7853acd3bffb6986e59c28d
L3viathan <git@l3vi.de>
2022-07-14 10:46:47 +0200

    Changes made during talk

--- slide3.py
+++ slide3.py
@@ -3,7 +3,9 @@ import sys
 x = 23
 
 def increment(variable):
-    ...
+    frame = sys._getframe()
+    frame = frame.f_back
+    frame.f_locals[variable] += 1
 
 increment("x")
 
--- slide4.py
+++ slide4.py
@@ -4,6 +4,12 @@ import sys
 def trace(frame, event, arg):
     # event types: call, line, return, exception, opcode
     print("Tracing", frame, "with", event)
+    return trace
 
 # set global trace function
 sys.settrace(trace)
+
+def add(x, y):
+    return x + y
+
+add(23, 42)
